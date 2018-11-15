dc4e4497fcb3ed125225af29ecca2a0e67becd4c
Ratansairohith <34803938+Ratansairohith@users.noreply.github.com>
2018-11-15 15:29:04 +0530

    Driver monitoring system

---
+++ Kshemin/app/src/main/java/com/example/Kshemin/drivelert/contactus.java
@@ -0,0 +1,20 @@
+package com.example.Kshemin.drivelert;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.akarshan.drivelert.R;
+
+public class contactus extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View root;
+        root = inflater.inflate(R.layout.activity_contactus,container,false);
+        return root;
+    }
+}
